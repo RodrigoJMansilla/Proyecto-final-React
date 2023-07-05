ac2ea66196dfd300d4f360096402f28377b4b871
Rodrigo Mansilla <mansillarodrigoj@gmail.com>
2023-07-05 11:46:49 -0300

    Estilados de CartCard

--- src/components/common/productCartCard/ProductCartCard.jsx
+++ src/components/common/productCartCard/ProductCartCard.jsx
@@ -1,13 +1,28 @@
+import { Button } from "@mui/material"
 import styles from "./ProductCartCard.module.css"
 
 export const ProductCartCard = ({producto}) => {
   return (
     <div className={styles.cartCard}>
+      <div className={styles.contImg}>
         <img className={styles.foto} src={producto.img} alt={producto.title} />
-        <h4 className={styles.texto}><strong>{producto.title}</strong></h4>
-        <h4 className={styles.texto}>{producto.quantity}</h4>
-        <h4 className={styles.texto}>{producto.price * producto.quantity}</h4>
-        <button>X</button>
+      </div>
+      <div className={styles.subCont}>
+        <div className={styles.subCont1}>
+          <h4 className={styles.texto}>{producto.title}</h4>
+        </div>
+        <div className={styles.subCont2}>
+          <h4 className={styles.texto}>Cantidad: {producto.quantity}</h4>
+        </div>
+        <div className={styles.subCont3}>
+          <h4 className={styles.texto}>${producto.price * producto.quantity}</h4>
+        </div>
+        <div className={styles.subCont4}>
+          <Button  color="error" size="small" variant="contained">X</Button>
+        </div>
+
+      </div>
+
     </div>
   )
 }
